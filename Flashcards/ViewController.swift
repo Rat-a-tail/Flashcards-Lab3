a02dd5d22a86958c9e06f72643201487c3cb58ea
Rat-a-tail <kinsum1@stolaf.edu>
2021-02-20 14:44:14 -0600

    Initial flashcard

--- Flashcards/ViewController.swift
+++ Flashcards/ViewController.swift
@@ -9,11 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var FrontLabel: UILabel!
+    @IBOutlet weak var BackLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func didTaponFlashcard(_ sender: Any) {
+    }
+    
 
 }
 
